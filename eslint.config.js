40b1e9fbef004c9a759071ed5ac2d7cb4749ede0
zxx-sqq <532721245@qq.com>
2026-05-05 16:04:57 +0800

    移除多余配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 ﻿import vuePlugin from 'eslint-plugin-vue';
 import vueParser from 'vue-eslint-parser';
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
 import prettierConfig from 'eslint-config-prettier';
 
 const commonRules = {
@@ -21,14 +19,10 @@ export default [
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
-      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
     },
     rules: {
       ...commonRules,
